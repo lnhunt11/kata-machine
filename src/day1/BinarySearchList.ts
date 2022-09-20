5f0314c263df0ae4d85d59083444ac034812d016
lnhunt11 <86940809+lnhunt11@users.noreply.github.com>
2022-09-20 12:45:43 -0700

    original day1

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,13 +1,3 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-  let lo = 0;
-  let hi = haystack.length;
-  do{
-    const m: number = Math.floor(lo + (hi - lo)/2);
-    const v: number = haystack[m];
 
-    if (v === needle) return true;
-    else if (needle > v) lo = m + 1;
-    else hi = m;
-  } while (lo < hi);
-  return false;
 }
