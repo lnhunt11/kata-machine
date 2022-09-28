369a9a4034251e0ad7a9c4d4a85d40e4ffe942b9
lnhunt11 <86940809+lnhunt11@users.noreply.github.com>
2022-09-28 12:26:38 -0700

    comparing BST

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,16 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+  //checking to see if we have made to both subtrees where we can no longer recurse further
+  if (a === null && b === null){
+    return true;
+  }
+  // One of us is null and the other isn't we have traverse to different structure
+  if (a === null || b === null){
+    return false;
+  }
+  // value check: 
+  if (a.value !== b.value){
+    return false;
+  }
 
+  return compare(a.left, b.left) && compare(a.right, b.right);
 }
